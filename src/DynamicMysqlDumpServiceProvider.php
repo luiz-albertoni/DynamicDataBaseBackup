d1a1fa8e1dfd863658feec60660a46d19a13bccd
luiz-albertoni <luiz.albertoni@gmail.com>
2017-08-14 16:54:58 -0300

    add vendor publish code for install

--- src/DynamicMysqlDumpServiceProvider.php
+++ src/DynamicMysqlDumpServiceProvider.php
@@ -7,10 +7,24 @@ use Albertoni\DynamicDataBaseBackup\DynamicMysqlDumpService;
 
 class DynamicMysqlDumpServiceProvider extends ServiceProvider
 {
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/dynamic-mysql-dump.php' => config_path('dynamic-mysql-dump'),
+        ], 'config');
+    }
+
     public function register()
     {
         $this->app->singleton('dynamicDump', function($app) {
             return new DynamicMysqlDumpService();
         });
     }
+
 }
